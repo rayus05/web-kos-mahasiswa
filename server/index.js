30e7bdb3c8c091973262118b5628696cca4cf44f
rayus05 <rhmtysf001@gmail.com>
2026-01-07 04:55:46 +0700

    Update

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ cloudinary.v2.config({
 // --- Middleware (Satpam/Perantara) ---
 app.use(cors({
   origin: [
-    "https://web-kos-mahasiswa-gp26.vercel.app", // URL Frontend Vercel Kamu (Sesuai Log Error)
+    "https://edukost.vercel.app", // URL Frontend Vercel Kamu (Sesuai Log Error)
     "http://localhost:5173" // Biar di laptop tetap jalan
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
